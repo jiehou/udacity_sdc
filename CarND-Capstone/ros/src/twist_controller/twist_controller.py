09f9eaf2032cee22267c240be0693643a3737287
jiehou <jie.hou85@gmail.com>
2019-03-03 12:48:49 -0800

    begin capstone project

---
+++ CarND-Capstone/ros/src/twist_controller/twist_controller.py
@@ -0,0 +1,37 @@
+import rospy
+from lowpass import LowPassFilter
+from pid import PID
+
+GAS_DENSITY = 2.858
+ONE_MPH = 0.44704
+
+
+class Controller(object):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit,
+        accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+        # TODO: Implement
+        self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn = 0. # minimum throttle value
+        mx = 0.2 # maximum throttle value
+        self.throttle_controller = PID(kp, ki, kp, mn, mx)
+
+        tau = 0.5 # 1/(2pi*tau) = cutoff frequencey
+        ts = 0.02 # sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+        self.last_time = rospy.get_time()
+
+
+    # *args, **kwargs
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+        # TODO: Change the arg, kwarg list to suit your needs
+        # Return throttle, brake, steer
+        return 1., 0., 0.
